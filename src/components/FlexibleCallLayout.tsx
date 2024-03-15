e50c16820183aa9a877b637f52bce00beda9aa4d
Anubhav Jain <anubhavjain.jain0@gmail.com>
2024-03-15 15:02:58 +0530

    video-call-layout

---
+++ src/components/FlexibleCallLayout.tsx
@@ -0,0 +1,75 @@
+import useStreamCall from "@/hooks/useStreamCall";
+import {
+  CallControls,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import {
+  BetweenHorizontalEnd,
+  BetweenVerticalEnd,
+  LayoutGrid,
+} from "lucide-react";
+import { useState } from "react";
+import EndCallButton from "./EndCallButton";
+import { useRouter } from "next/navigation";
+
+type CallLayout = "speaker-vert" | "speaker-horiz" | "grid";
+
+export default function FlexibleCallLayout() {
+  const [layout, setLayout] = useState<CallLayout>("speaker-vert");
+  const call = useStreamCall();
+
+  const router = useRouter();
+  return (
+    <div className="space-y-3">
+      <CallLayoutButton layout={layout} setLayout={setLayout} />
+      <CallLayoutView layout={layout} />
+      <CallControls onLeave={() => router.push(`/meeting/${call.id}/left`)} />
+      <EndCallButton />
+    </div>
+  );
+}
+
+interface CallLayoutButtonProps {
+  layout: CallLayout;
+  setLayout: (layout: CallLayout) => void;
+}
+
+function CallLayoutButton({ layout, setLayout }: CallLayoutButtonProps) {
+  return (
+    <div className="mx-auto w-fit space-x-6">
+      <button onClick={() => setLayout("speaker-vert")}>
+        <BetweenVerticalEnd
+          className={layout !== "speaker-vert" ? "text-gray-400" : ""}
+        />
+      </button>
+      <button onClick={() => setLayout("speaker-horiz")}>
+        <BetweenHorizontalEnd
+          className={layout !== "speaker-horiz" ? "text-gray-400" : ""}
+        />
+      </button>
+      <button onClick={() => setLayout("grid")}>
+        <LayoutGrid className={layout !== "grid" ? "text-gray-400" : ""} />
+      </button>
+    </div>
+  );
+}
+
+interface CallLayoutViewProps {
+  layout: CallLayout;
+}
+
+function CallLayoutView({ layout }: CallLayoutViewProps) {
+  if (layout === "grid") {
+    return <PaginatedGridLayout />;
+  }
+
+  if (layout === "speaker-vert") {
+    return <SpeakerLayout />;
+  }
+
+  if (layout === "speaker-horiz") {
+    return <SpeakerLayout participantsBarPosition="right" />;
+  }
+  return null;
+}
